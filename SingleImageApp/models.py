295cbb3ec057ebcdbb1d2676e260789aebb700e1
dhanushkomari <dhanushkomari@gmail.com>
2021-08-19 13:12:19 +0530

    single-image-upload-model-added

--- SingleImageApp/models.py
+++ SingleImageApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import CustomUser as User
 
 # Create your models here.
+
+
+class SingleImage(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    input_image = models.ImageField(upload_to = 'SingleInputImages/')
+    result_image = models.ImageField(upload_to = 'SingleResultImages/')
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return '{}'.format(self.user)
+
+    class Meta:
+        verbose_name = 'Uploaded Single Image'
+        verbose_name_plural = 'Uploaded Single Images'
+        ordering = ('-created_at',)
+
+    def get_url(self):
+        pass
+        # return reverse()
+
